b83437a60f458841d2917f00502ec7afd8d6996a
taggon <gonom9@gmail.com>
2022-12-28 06:56:26 +0900

    애니메이션 검색 예제 업데이트

--- src/anime/main.js
+++ src/anime/main.js
@@ -1,26 +1,16 @@
+/**
+ * 애니메이션 검색
+ *
+ * 사용할 API:
+ * - url: `https://api.jikan.moe/v4/anime?q=${query}&page=1&limit=10`
+ * - query: 검색어
+ *
+ * API 문서: https://docs.api.jikan.moe/#tag/anime/operation/getAnimeSearch
+ */
+
 const results = document.querySelector('#results');
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const query = event.target.query.value;
-  fetch(`https://api.jikan.moe/v4/anime?q=${query}&page=1&limit=10`)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-      results.innerHTML = '';
-      result.data?.forEach(anime => {
-        const tpl = document.querySelector('template');
-        const card = tpl.content.cloneNode(true);
-        const img = (anime.images.webp ?? anime.images.jpg).image_url;
-
-        card.querySelector('h2').innerText = anime.title;
-        card.querySelector('img').src = img;
-        card.querySelector('p').innerText = anime.synopsis ?? 'No synopsis';
-
-        results.appendChild(card);
-      });
-    })
-    .catch(err => console.err(err));
+  // TODO
 }, false);
